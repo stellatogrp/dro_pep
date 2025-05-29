0933c8648f9660037194d733e3fac878dc5c9788
Vinit Ranjan <vinitranjan821@gmail.com>
2025-05-29 03:12:33 -0400

    added time plots

---
+++ src/experiment_classes/lasso.py
@@ -0,0 +1,82 @@
+import numpy as np
+from PEPit import PEP
+from PEPit.functions import (
+    SmoothStronglyConvexFunction,
+    SmoothStronglyConvexQuadraticFunction,
+    ConvexFunction,
+    ConvexLipschitzFunction,
+)
+from PEPit.primitive_steps import proximal_step
+from PEPit.tools.expressions_to_matrices import expression_to_matrices
+
+
+def test_pep():
+    problem = PEP()
+    mu = 0.1
+    L = 1
+    gamma = 0.5 / L
+    K = 1
+    lambd = 0.1
+
+    # Declare a strongly convex smooth function and a closed convex proper function
+    f1 = problem.declare_function(SmoothStronglyConvexQuadraticFunction, mu=mu, L=L)
+    # f1 = problem.declare_function(SmoothStronglyConvexFunction, mu=mu, L=L)
+    # f2 = problem.declare_function(ConvexFunction)
+    f2 = problem.declare_function(ConvexLipschitzFunction, M=lambd)
+    func = f1 + f2
+
+    # Start by defining its unique optimal point xs = x_*
+    xs = func.stationary_point()
+
+    # Then define the starting point x0 of the algorithm
+    x0 = problem.set_initial_point()
+    g0 = f1.gradient(x0)
+    f0 = func(x0)
+
+    # Set the initial constraint that is the distance between x0 and x^*
+    problem.set_initial_condition((x0 - xs) ** 2 <= 4)
+
+    # Run the proximal gradient method starting from x0
+
+    x = [x0 for _ in range(K+1)]
+    g = [g0 for _ in range(K+1)]
+    f = [f0 for _ in range(K+1)]
+
+    # x = x0
+    for k in range(K):
+        # y = x - gamma * f1.gradient(x)
+        # x, gx, fx = proximal_step(y, f2, gamma)
+
+        y = x[k] - gamma * f1.gradient(x[k])
+        x[k+1], g[k+1], f[k+1] = proximal_step(y, f2, gamma)
+
+    # Set the performance metric to the distance between x and xs
+    # problem.set_performance_metric((x - xs) ** 2)
+    # problem.set_performance_metric(func(x0) - func(xs))
+    problem.set_performance_metric(f[-1] - func(xs))
+
+    # Solve the PEP
+    # pepit_tau = problem.solve(wrapper=wrapper, solver=solver, verbose=pepit_verbose)
+
+    # problem.set_initial_condition(x[1] ** 2 <= 9)
+    # problem.set_initial_condition(func(x[1]) <= 9)
+    # problem.set_initial_condition(f2.gradient(xs) ** 2 <= 5)
+
+    pepit_tau = problem.solve(wrapper='cvxpy', solver='CLARABEL')
+
+    print(pepit_tau)
+
+    A_obj, b_obj, _ = expression_to_matrices(problem._list_of_constraints_sent_to_wrapper[0].expression)
+    print(A_obj, b_obj)
+    print(A_obj.shape, b_obj.shape)
+
+    print(len(problem._list_of_constraints_sent_to_wrapper))
+    for constr in problem._list_of_constraints_sent_to_wrapper[1:]:
+        A_cons, b_cons, c_cons = expression_to_matrices(constr.expression)
+
+        print(A_cons, b_cons, c_cons)
+
+
+
+if __name__ == '__main__':
+    test_pep()
---
+++ src/experiment_plots/Huber/plot_times.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    # "font.sans-serif": ["Helvetica Neue"],
+    "font.size": 14,
+    "figure.figsize": (10, 6),
+})
+
+pep_K_max = 50
+exp_K_max = 50
+cvar_K_max = 50
+
+GD_pep = pd.read_csv('data/pep/grad_desc_1_68/pep.csv')
+NGD_pep = pd.read_csv('data/pep/nesterov_grad_desc_1_50/pep.csv')
+
+GD_exp_dro = pd.read_csv('data/dro/grad_desc_exp_1_50/dro.csv')
+GD_cvar_dro = pd.read_csv('data/dro/grad_desc_cvar_1_50/dro.csv')
+NGD_exp_dro = pd.read_csv('data/dro/nesterov_grad_desc_exp_1_50/dro.csv')
+NGD_cvar_dro = pd.read_csv('data/dro/nesterov_grad_desc_cvar_1_50/dro.csv')
+
+
+def plot_times():
+    GD_pep_times = GD_pep[GD_pep['obj'] == 'obj_val']['solvetime'][:pep_K_max]
+    NGD_pep_times = NGD_pep[NGD_pep['obj'] == 'obj_val']['solvetime'][:pep_K_max]
+
+    GD_exp_times = GD_exp_dro.groupby(['K'])['solvetime'].mean().iloc[:exp_K_max]
+    GD_cvar_times = GD_cvar_dro.groupby(['K'])['solvetime'].mean().iloc[:exp_K_max]
+    NGD_exp_times = NGD_exp_dro.groupby(['K'])['solvetime'].mean().iloc[:exp_K_max]
+    NGD_cvar_times = NGD_cvar_dro.groupby(['K'])['solvetime'].mean().iloc[:exp_K_max]
+
+    fig, ax = plt.subplots(1, 2)
+    ax[0].set_xlabel(r'$K$')
+    ax[1].set_xlabel(r'$K$')
+    ax[0].set_ylabel('Solve time (s)')
+
+    ax[1].sharey(ax[0])
+
+    ax[0].set_yscale('log')
+
+    ax[0].set_title('GD')
+    ax[1].set_title('AGD')
+
+    ax[0].plot(range(1, pep_K_max + 1), GD_pep_times, label='PEP')
+    ax[0].plot(range(1, exp_K_max + 1), GD_exp_times, label='Exp')
+    ax[0].plot(range(1, cvar_K_max + 1), GD_cvar_times, label='CVar')
+
+    ax[1].plot(range(1, pep_K_max + 1), NGD_pep_times, label='PEP')
+    ax[1].plot(range(1, exp_K_max + 1), NGD_exp_times, label='Exp')
+    ax[1].plot(range(1, cvar_K_max + 1), NGD_cvar_times, label='CVar')
+
+    ax[0].legend()
+    # plt.show()
+
+    plt.savefig('huber_times.pdf')
+
+
+if __name__ == '__main__':
+    plot_times()
