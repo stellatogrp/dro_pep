b5c78d39c47f7983c188d7f1490c2d7448ff3e22
Vinit Ranjan <vinitranjan821@gmail.com>
2025-08-27 04:03:18 -0400

    adjusted all timing plots across all experiments

---
+++ src/experiment_plots/Lasso/plot_times.py
@@ -0,0 +1,107 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    # "font.sans-serif": ["Helvetica Neue"],
+    "font.size": 14,
+    "figure.figsize": (10, 6),
+})
+
+pep_K_max = 25
+exp_K_max = 25
+cvar_K_max = 25
+
+ISTA_samples = pd.read_csv('data/samples/ISTA_1_40/samples.csv')
+FISTA_samples = pd.read_csv('data/samples/FISTA_1_40/samples.csv')
+OptISTA_samples = pd.read_csv('data/samples/OptISTA_1_40/samples.csv')
+
+ISTA_pep = pd.read_csv('data/pep/ISTA_1_40/pep.csv')
+FISTA_pep = pd.read_csv('data/pep/FISTA_1_40/pep.csv')
+OptISTA_pep = pd.read_csv('data/pep/OptISTA_1_40/pep.csv')
+
+ISTA_exp_dro = pd.read_csv('data/dro/ISTA_exp_1_25/dro.csv')
+ISTA_cvar_dro = pd.read_csv('data/dro/ISTA_cvar_1_25/dro.csv')
+FISTA_exp_dro = pd.read_csv('data/dro/FISTA_exp_1_25/dro.csv')
+FISTA_cvar_dro = pd.read_csv('data/dro/FISTA_cvar_1_25/dro.csv')
+OptISTA_exp_dro = pd.read_csv('data/dro/OptISTA_exp_1_25/dro.csv')
+OptISTA_cvar_dro = pd.read_csv('data/dro/OptISTA_cvar_1_25/dro.csv')
+
+
+def plot_times():
+    ISTA_color = 'tab:blue'
+    FISTA_color = 'tab:green'
+    OptISTA_color = 'tab:red'
+
+    ISTA_pep_times = ISTA_pep[ISTA_pep['obj'] == 'obj_val']['solvetime'][:pep_K_max]
+    FISTA_pep_times = FISTA_pep[FISTA_pep['obj'] == 'obj_val']['solvetime'][:pep_K_max]
+    OptISTA_pep_times = OptISTA_pep[OptISTA_pep['obj'] == 'obj_val']['solvetime'][:pep_K_max]
+
+    ISTA_exp_times = ISTA_exp_dro.groupby(['K'])['solvetime'].mean().iloc[:exp_K_max]
+    ISTA_cvar_times = ISTA_cvar_dro.groupby(['K'])['solvetime'].mean().iloc[:exp_K_max]
+    FISTA_exp_times = FISTA_exp_dro.groupby(['K'])['solvetime'].mean().iloc[:exp_K_max]
+    FISTA_cvar_times = FISTA_cvar_dro.groupby(['K'])['solvetime'].mean().iloc[:exp_K_max]
+    OptISTA_exp_times = OptISTA_exp_dro.groupby(['K'])['solvetime'].mean().iloc[:exp_K_max]
+    OptISTA_cvar_times = OptISTA_cvar_dro.groupby(['K'])['solvetime'].mean().iloc[:exp_K_max]
+
+    fig, ax = plt.subplots(1, 3)
+
+    ax[0].set_ylabel('Solve time(s)')
+    ax[0].set_yscale('log')
+    ax[1].set_yscale('log')
+    ax[2].set_yscale('log')
+    # ax[1].set_yscale('log')
+    # ax[0].set_xscale('log')
+    # ax[1].set_xscale('log')
+
+    ax[0].grid(color='lightgray', alpha=0.3)
+    ax[1].grid(color='lightgray', alpha=0.3)
+    ax[2].grid(color='lightgray', alpha=0.3)
+
+    ax[0].set_xticks([5, 10, 15, 20, 25])
+
+    ax[1].sharey(ax[0])
+    ax[2].sharey(ax[0])
+
+    ax[1].sharex(ax[0])
+    ax[2].sharex(ax[0])
+
+    ax[0].set_xlabel(r'$K$')
+    ax[1].set_xlabel(r'$K$')
+
+    ax[0].set_title('Worst-case')
+    ax[2].set_title('Expectation')
+    ax[1].set_title('CVar')
+
+    ax[0].plot(range(1, exp_K_max + 1), ISTA_pep_times, label='ISTA', color=ISTA_color)
+    ax[0].plot(range(1, exp_K_max + 1), FISTA_pep_times, label='FISTA', color=FISTA_color)
+    ax[0].plot(range(1, exp_K_max + 1), OptISTA_pep_times, label='OptISTA', color=OptISTA_color)
+
+    ax[2].plot(range(1, exp_K_max + 1), ISTA_exp_times, label='ISTA', color=ISTA_color)
+    ax[2].plot(range(1, exp_K_max + 1), FISTA_exp_times, label='FISTA', color=FISTA_color)
+    ax[2].plot(range(1, exp_K_max + 1), OptISTA_exp_times, label='OptISTA', color=OptISTA_color)
+
+    ax[1].plot(range(1, exp_K_max + 1), ISTA_cvar_times, label='ISTA', color=ISTA_color)
+    ax[1].plot(range(1, exp_K_max + 1), FISTA_cvar_times, label='FISTA', color=FISTA_color)
+    ax[1].plot(range(1, exp_K_max + 1), OptISTA_cvar_times, label='OptISTA', color=OptISTA_color)
+
+    for axi in ax:
+        box = axi.get_position()
+        # x0, y0, x1, y1 = bbox.x0, bbox.y0, bbox.x1, bbox.y1
+        axi.set_position([box.x0, box.y0+.05, box.width, box.height-.05])
+
+    # ax[0].legend()
+    handles, labels = ax[0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='lower center', ncols=4)
+
+    plt.suptitle('Lasso Minimization, Solve times')
+
+    # plt.show()
+
+    plt.savefig('lasso_times.pdf')
+
+
+if __name__ == '__main__':
+    plot_times()
--- src/experiment_plots/Quad_objval/plot_times.py
+++ src/experiment_plots/Quad_objval/plot_times.py
@@ -10,52 +10,87 @@ plt.rcParams.update({
     "figure.figsize": (10, 6),
 })
 
-pep_K_max = 50
-exp_K_max = 50
-cvar_K_max = 50
+pep_K_max = 30
+exp_K_max = 30
+cvar_K_max = 30
 
-GD_pep = pd.read_csv('data/pep/grad_desc/pep.csv')
-NGD_pep = pd.read_csv('data/pep/nesterov_grad_desc/pep.csv')
+GD_pep = pd.read_csv('data/pep/grad_desc_1_40/pep.csv')
+NGD_pep = pd.read_csv('data/pep/nesterov_grad_desc_1_40/pep.csv')
 
-GD_exp_dro = pd.read_csv('data/dro/grad_desc_exp_1_50/dro.csv')
-GD_cvar_dro = pd.read_csv('data/dro/grad_desc_cvar_1_50/dro.csv')
-NGD_exp_dro = pd.read_csv('data/dro/nesterov_grad_desc_exp_1_50/dro.csv')
-NGD_cvar_dro = pd.read_csv('data/dro/nesterov_grad_desc_cvar_1_50/dro.csv')
+GD_exp_dro = pd.read_csv('data/dro/grad_desc_exp_1_40/dro.csv')
+GD_cvar_dro = pd.read_csv('data/dro/grad_desc_cvar_1_40/dro.csv')
+NGD_exp_dro = pd.read_csv('data/dro/nesterov_grad_desc_exp_1_40/dro.csv')
+NGD_cvar_dro = pd.read_csv('data/dro/nesterov_grad_desc_cvar_1_40/dro.csv')
+
+PEP_OBJ = 'obj_val'
 
 
 def plot_times():
-    GD_pep_times = GD_pep[GD_pep['obj'] == 'obj_val']['solvetime'][:pep_K_max]
-    NGD_pep_times = NGD_pep[NGD_pep['obj'] == 'obj_val']['solvetime'][:pep_K_max]
+    GD_color = 'tab:blue'
+    NGD_color = 'tab:green'
+
+    GD_pep_times = GD_pep[GD_pep['obj'] == PEP_OBJ]['solvetime'][:pep_K_max]
+    NGD_pep_times = NGD_pep[NGD_pep['obj'] == PEP_OBJ]['solvetime'][:pep_K_max]
 
     GD_exp_times = GD_exp_dro.groupby(['K'])['solvetime'].mean().iloc[:exp_K_max]
     GD_cvar_times = GD_cvar_dro.groupby(['K'])['solvetime'].mean().iloc[:exp_K_max]
     NGD_exp_times = NGD_exp_dro.groupby(['K'])['solvetime'].mean().iloc[:exp_K_max]
     NGD_cvar_times = NGD_cvar_dro.groupby(['K'])['solvetime'].mean().iloc[:exp_K_max]
 
-    fig, ax = plt.subplots(1, 2)
-    ax[0].set_xlabel(r'$K$')
-    ax[1].set_xlabel(r'$K$')
-    ax[0].set_ylabel('Solve time (s)')
+    # ax[0].legend()
+
+    fig, ax = plt.subplots(1, 3)
+
+    ax[0].set_ylabel('Solve time(s)')
+    ax[0].set_yscale('log')
+    ax[1].set_yscale('log')
+    ax[2].set_yscale('log')
+    # ax[1].set_yscale('log')
+    # ax[0].set_xscale('log')
+    # ax[1].set_xscale('log')
+
+    ax[0].grid(color='lightgray', alpha=0.3)
+    ax[1].grid(color='lightgray', alpha=0.3)
+    ax[2].grid(color='lightgray', alpha=0.3)
+
+    ax[0].set_xticks([10, 20, 30, 40])
 
     ax[1].sharey(ax[0])
+    ax[2].sharey(ax[0])
 
-    ax[0].set_yscale('log')
+    ax[1].sharex(ax[0])
+    ax[2].sharex(ax[0])
 
-    ax[0].set_title('GD')
-    ax[1].set_title('AGD')
+    ax[0].set_xlabel(r'$K$')
+    ax[1].set_xlabel(r'$K$')
 
-    ax[0].plot(range(1, pep_K_max + 1), GD_pep_times, label='PEP')
-    ax[0].plot(range(1, exp_K_max + 1), GD_exp_times, label='Exp')
-    ax[0].plot(range(1, cvar_K_max + 1), GD_cvar_times, label='CVar')
+    ax[0].set_title('Worst-case')
+    ax[2].set_title('Expectation')
+    ax[1].set_title('CVar')
 
-    ax[1].plot(range(1, pep_K_max + 1), NGD_pep_times, label='PEP')
-    ax[1].plot(range(1, exp_K_max + 1), NGD_exp_times, label='Exp')
-    ax[1].plot(range(1, cvar_K_max + 1), NGD_cvar_times, label='CVar')
+    ax[0].plot(range(1, exp_K_max + 1), GD_pep_times, label='GD', color=GD_color)
+    ax[0].plot(range(1, exp_K_max + 1), NGD_pep_times, label='NGD', color=NGD_color)
+    
+    ax[2].plot(range(1, exp_K_max + 1), GD_exp_times, label='GD', color=GD_color)
+    ax[2].plot(range(1, exp_K_max + 1), NGD_exp_times, label='NGD', color=NGD_color)
 
-    ax[0].legend()
+    ax[1].plot(range(1, cvar_K_max + 1), GD_cvar_times, label='GD', color=GD_color)
+    ax[1].plot(range(1, cvar_K_max + 1), NGD_cvar_times, label='NGD', color=NGD_color)
+
+    for axi in ax:
+        box = axi.get_position()
+        # x0, y0, x1, y1 = bbox.x0, bbox.y0, bbox.x1, bbox.y1
+        axi.set_position([box.x0, box.y0+.05, box.width, box.height-.05])
+
+    # ax[0].legend()
+    handles, labels = ax[0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='lower center', ncols=4)
+
+    plt.suptitle('Quadratic Objective Value Minimization, Solve times')
+
     # plt.show()
 
-    plt.savefig('quad_times.pdf')
+    plt.savefig('quad_obj_val_times.pdf')
 
 
 if __name__ == '__main__':
