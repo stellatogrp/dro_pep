f17ffd863bf3be4f065c5c33eb5321384afb7fda
Vinit Ranjan <vinitranjan821@gmail.com>
2025-11-03 15:25:35 -0500

    updated bounding plots

--- src/experiment_plots/LogReg/plot_bounds.py
+++ src/experiment_plots/LogReg/plot_bounds.py
@@ -10,8 +10,8 @@ plt.rcParams.update({
     "figure.figsize": (12, 6),
 })
 
-exp_K_max = 10
-cvar_K_max = 10
+exp_K_max = 30
+cvar_K_max = 30
 
 num_eps_vals = 5
 
@@ -90,9 +90,9 @@ def main_bounds():
     NGD_color = 'tab:orange'
 
     GD_exp_dro_eps = GD_exp_dro[GD_exp_dro['eps_idx'] == 6]
-    GD_cvar_dro_eps = GD_cvar_dro[GD_cvar_dro['eps_idx'] == 4]
+    GD_cvar_dro_eps = GD_cvar_dro[GD_cvar_dro['eps_idx'] == 6]
     NGD_exp_dro_eps = NGD_exp_dro[NGD_exp_dro['eps_idx'] == 6]
-    NGD_cvar_dro_eps = NGD_cvar_dro[NGD_cvar_dro['eps_idx'] == 4]
+    NGD_cvar_dro_eps = NGD_cvar_dro[NGD_cvar_dro['eps_idx'] == 6]
 
     GD_worst_k = []
     NGD_worst_k = []
@@ -173,8 +173,8 @@ def main_bounds():
 
     plt.suptitle('Logistic Regression, Objective Value')
 
-    plt.show()
-    # plt.savefig(f'logreg_obj_val.pdf')
+    # plt.show()
+    plt.savefig(f'logreg_obj_val.pdf')
 
 
 if __name__ == '__main__':
--- src/experiment_plots/Quad_NonstrongCvx/plot_bounds.py
+++ src/experiment_plots/Quad_NonstrongCvx/plot_bounds.py
@@ -10,8 +10,8 @@ plt.rcParams.update({
     "figure.figsize": (12, 6),
 })
 
-exp_K_max = 10
-cvar_K_max = 10
+exp_K_max = 30
+cvar_K_max = 30
 
 num_eps_vals = 5
 
@@ -96,10 +96,10 @@ def main_bounds():
     GD_color = 'tab:blue'
     NGD_color = 'tab:orange'
 
-    GD_exp_dro_eps = GD_exp_dro[GD_exp_dro['eps_idx'] == 6]
-    GD_cvar_dro_eps = GD_cvar_dro[GD_cvar_dro['eps_idx'] == 4]
-    NGD_exp_dro_eps = NGD_exp_dro[NGD_exp_dro['eps_idx'] == 6]
-    NGD_cvar_dro_eps = NGD_cvar_dro[NGD_cvar_dro['eps_idx'] == 4]
+    GD_exp_dro_eps = GD_exp_dro[GD_exp_dro['eps_idx'] == 5]
+    GD_cvar_dro_eps = GD_cvar_dro[GD_cvar_dro['eps_idx'] == 3]
+    NGD_exp_dro_eps = NGD_exp_dro[NGD_exp_dro['eps_idx'] == 5]
+    NGD_cvar_dro_eps = NGD_cvar_dro[NGD_cvar_dro['eps_idx'] == 2]
 
     GD_exp_gamma = GD_exp_fit_params['gamma'].loc[0]
     GD_exp_C = GD_exp_fit_params['C'].loc[0]
@@ -203,8 +203,8 @@ def main_bounds():
 
     plt.suptitle('Nonstrongly Convex Quadratic Minimization, Distance to Optimality')
 
-    plt.show()
-    # plt.savefig(f'quad_nonstrongcvx.pdf')
+    # plt.show()
+    plt.savefig(f'quad_nonstrongcvx.pdf')
 
 
 if __name__ == '__main__':
