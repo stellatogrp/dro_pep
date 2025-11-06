2f2aefc8e93b6c10b0716ab9013f041758eebb05
Jisun Park <jisunp515@gmail.com>
2025-11-06 08:39:59 -0500

    box_pgd plot changed
    Now the setup as in paper figure

--- src/experiment_plots/SimpleLstsq/plot_lstsq.py
+++ src/experiment_plots/SimpleLstsq/plot_lstsq.py
@@ -6,31 +6,34 @@ plt.rcParams.update({
     "text.usetex": True,
     "font.family": "serif",
     # "font.sans-serif": ["Helvetica Neue"],
-    "font.size": 14,
+    "font.size": 17, # default: 14
     "figure.figsize": (9, 6),
+    "lines.linewidth": 2.5,
 })
 
 
 def main():
-    avg_color = 'tab:blue'
-    pep_color = 'tab:green'
-    sm_color = 'tab:orange'
-    p90_color = 'tab:gray'
+    # colorblind-safe: #D81B60 (magenta) #FFC107 (yellow) #004D40 (dark green) #1E88E5 (blue)
+    pep_color = 'tab:gray' # 'tag:green'
+    # sm_color = 'tab:orange'
+    p90_color = 'k'  # 'tab:gray'
+    # avg_color = 'tab:blue'
+    avg_color = 'tab:green' # 'tab:blue'
 
     fig, ax = plt.subplots()
     sample_df = pd.read_csv('data/samples.csv', header=None)
     theory_df = pd.read_csv('data/theory.csv', header=None)
-    scaled_theory_df = pd.read_csv('data/scaled_theory.csv', header=None)
+    # scaled_theory_df = pd.read_csv('data/scaled_theory.csv', header=None)
     samples = sample_df.to_numpy()
     theory = theory_df.to_numpy()
-    scaled_theory = scaled_theory_df.to_numpy()
+    # scaled_theory = scaled_theory_df.to_numpy()
     
     ax.set_yscale('log')
     # ax.set_xscale('log')
 
     # ax.set_ylabel(r'$\| x^K - x^\star\|$')
-    # ax.set_ylabel(r'$f(x^K) - f(x^\star)$')
-    ax.set_ylabel(r'$f(z^K) - f(z^\star)$')
+    ax.set_ylabel(r'$f(x^K) - f(x^\star)$')
+    # ax.set_ylabel(r'$f(z^K) - f(z^\star)$')
     ax.set_xlabel(r'$K$')
 
     average = np.mean(samples, axis=0)
@@ -42,17 +45,18 @@ def main():
     K_max = 100
     K_vals = range(1, K_max+1)
 
-    plt.plot(K_vals, average, label='Sample average', color=avg_color, linestyle='dashed')
-    plt.plot(K_vals, quantiles, label='Sample 90th percentile', color=p90_color, linestyle='dotted')
-    plt.plot(K_vals, sample_max, label='Sample worst-case', color=sm_color, linestyle='dashdot')
-    plt.plot(K_vals, theory, label='Theoretical worst-case', color=pep_color)
+    worst_case,  = ax.plot(K_vals, theory, label='Theoretical worst-case', color=pep_color)
+    # plt.plot(K_vals, sample_max, label='Sample worst-case', color=sm_color, linestyle='dashdot')
+    percentile, = ax.plot(K_vals, quantiles, label='Sample 90th percentile', color=p90_color, linestyle='dotted')
+    average, = ax.plot(K_vals, average, label='Sample average', color=avg_color, linestyle='dashed')
     # plt.plot(K_vals, scaled_theory)
-    plt.legend()
+
+    ax.legend(handles=[worst_case, percentile, average])
 
     ax.grid(color='lightgray', alpha=0.3)
 
     # plt.show()
-    plt.savefig('box_pgd.pdf')
+    plt.savefig('box_pgd_new2.pdf')
 
 
 if __name__ == '__main__':
