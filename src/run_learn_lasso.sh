157e63fb7bb45c4d64f1df9d146283f0a74067e2
Vinit Ranjan <vranjan@Vinits-MacBook-Pro.local>
2026-01-20 01:45:19 -0500

    added lasso sh file

---
+++ src/run_learn_lasso.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=Lasso
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=16G
+#SBATCH --time=00-03:59:59
+#SBATCH --array=0-215
+#SBATCH -o /scratch/gpfs/BSTELLATO/vranjan/learn_dro_pep_out/Lasso/runs/%A.txt
+#SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
+#SBATCH --mail-user=vranjan@princeton.edu
+# #SBATCH --gres=gpu:1
+
+# os.environ['XLA_PYTHON_CLIENT_PREALLOCATE']='true'
+# export XLA_PYTHON_CLIENT_MEM_FRACTION='0.30'
+# export XLA_PYTHON_CLIENT_ALLOCATOR=platform
+# export xla_force_host_platform_device_count=1
+
+module purge
+module load anaconda3/2024.10
+# module load anaconda3/2023.9 cudnn/cuda-11.x/8.2.0 cudatoolkit/11.3 nvhpc/21.5
+conda activate algover
+
+python run_learning_experiment.py Lasso cluster
